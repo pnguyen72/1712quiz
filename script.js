bc7de2d3029622caba23e9850d49784353af0093
Felix <felix.fourcolor@gmail.com>
2024-10-14 23:26:06 -0700

    sort-of-working quiz generator

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const modules = {}
+const modules = []
 for (let i = 0; i < 6; i++) {
     fetch(`./data/quiz-data${i + 1}.json`)
         .then((response) => response.json())
-        .then((json) => modules[i] = json);
+        .then((data) => modules[i] = data);
 }
 
 const welcomePage = document.getElementById("welcome-page");
 const quizPage = document.getElementById("quiz-page");
+const nextButton = document.getElementById("next-btn");
+const submitButton = document.getElementById("submit-btn");
+const homeButon = document.getElementById("return-btn");
 const quizSelectionForm = document.getElementById("quiz-selection");
-const selectAllBox = document.getElementById("moduleALL")
+const questionNum = document.getElementById("questionNum");
+const moduleAllSelectBox = document.getElementById("moduleAllSelectBox")
 const moduleSelectBox = [
-    document.getElementById("module1"),
-    document.getElementById("module2"),
-    document.getElementById("module3"),
-    document.getElementById("module4"),
-    document.getElementById("module5"),
-    document.getElementById("module6"),
+    document.getElementById("module1SelectBox"),
+    document.getElementById("module2SelectBox"),
+    document.getElementById("module3SelectBox"),
+    document.getElementById("module4SelectBox"),
+    document.getElementById("module5SelectBox"),
+    document.getElementById("module6SelectBox"),
 ]
 
 function hideElement(element) {
@@ -27,32 +31,111 @@ function showElement(element) {
 }
 
 hideElement(quizPage);
+hideElement(submitButton)
 
 function selectAll() {
-    moduleSelectBox.forEach((element) => element.checked = selectAllBox.checked);
+    moduleSelectBox.forEach((element) => element.checked = moduleAllSelectBox.checked);
 }
 
 function selectOther() {
-    selectAllBox.checked = false;
+    moduleAllSelectBox.checked = false;
 }
 
-let quiz = {};
+let quizData = {};
 
-function startQuiz() {
-    hideElement(welcomePage);
-    showElement(quizPage);
-    for (let i = 0; i < 6; i++) {
-        if (moduleSelectBox[i].checked) {
-            quiz = { ...quiz, ...modules[i] }
-        }
-    }
-    quiz = Object.entries(quiz)
-    console.log(quiz)
-}
 
-function returnHome() {
+homeButon.addEventListener('click', (event) => {
+    showElement(nextButton);
     hideElement(quizPage);
     showElement(welcomePage);
+
     quizSelectionForm.reset();
-    quiz = {};
+    quizData = {};
+    document.getElementById("quiz").remove();
+});
+
+nextButton.addEventListener("click", (event) => {
+    for (let i = 0; i < 6; i++) {
+        if (moduleSelectBox[i].checked) {
+            quizData = { ...quizData, ...modules[i] }
+        }
+    }
+    quizData = Object.entries(quizData)
+
+    if (quizData.length == 0) return;
+
+    hideElement(nextButton);
+    hideElement(welcomePage);
+    showElement(quizPage);
+
+    shuffle(quizData)
+    quizPage.appendChild(generateQuiz());
+});
+
+function shuffle(array) {
+    if (array[0][0] == "True") return;
+
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        --currentIndex;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+}
+
+
+function generateQuiz() {
+    const number = questionNum.value
+    if (number == "ALL") {
+        sample = quizData
+    } else {
+        sample = quizData.slice(0, number);
+    }
+
+    div = document.createElement("div")
+    div.id = "quiz"
+    sample.forEach((question, questionIndex) => {
+        div.appendChild(generateQuestion(question, questionIndex))
+    });
+    return div;
+}
+
+function generateQuestion(question, questionIndex) {
+    const questionText = question[0];
+    const choices = Object.entries(question[1].choices)
+    const isMultiSelect = question[1].multi_select;
+
+    const div = document.createElement("div");
+    div.id = "question";
+
+    const p = document.createElement("p")
+
+    const title = document.createElement("b");
+    title.appendChild(document.createTextNode("Question " + (questionIndex + 1) + ". "));
+    p.appendChild(title);
+    p.appendChild(document.createTextNode(questionText));
+    div.appendChild(p)
+
+    choices.forEach((choice, choiceIndex) => {
+        const choiceText = choice[0];
+        const isCorrect = choice[1];
+
+        const input = document.createElement("input")
+        input.type = isMultiSelect ? "checkbox" : "radio";
+        input.name = "Q" + questionIndex;
+        input.id = input.name + "/" + choiceIndex;
+        input.setAttribute("class", isCorrect ? "correct" : "incorrect");
+        div.appendChild(input);
+
+        const label = document.createElement("label");
+        label.setAttribute("for", input.id);
+        label.setAttribute("class", isCorrect ? "correct" : "incorrect");
+        label.appendChild(document.createTextNode(choiceText));
+        div.appendChild(label);
+
+        div.appendChild(document.createElement("br"));
+        div.appendChild(document.createElement("br"));
+    });
+
+    return div;
 }
