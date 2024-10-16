9d60c66e609a725ddb1d58839ec9d19958d81609
Felix <felix.fourcolor@gmail.com>
2024-10-15 21:43:11 -0700

    sort-of-working grading system

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ for (let i = 0; i < 6; i++) {
 
 const welcomePage = document.getElementById("welcome-page");
 const quizPage = document.getElementById("quiz-page");
+const resultPanel = document.getElementById("result-panel");
 const nextButton = document.getElementById("next-btn");
 const submitButton = document.getElementById("submit-btn");
 const homeButon = document.getElementById("return-btn");
@@ -27,14 +28,18 @@ function hideElement(element) {
 }
 
 function showElement(element) {
-    element.style.display = "initial";
+    element.style.display = "flex";
 }
+
 
 hideElement(quizPage);
 hideElement(submitButton)
+hideElement(resultPanel);
 
 function selectAll() {
-    moduleSelectBox.forEach((element) => element.checked = moduleAllSelectBox.checked);
+    moduleSelectBox.forEach(
+        (element) => element.checked = moduleAllSelectBox.checked
+    );
 }
 
 function selectOther() {
@@ -42,19 +47,22 @@ function selectOther() {
 }
 
 let quizData = {};
-
+let attemptNum = 1;
 
 homeButon.addEventListener('click', (event) => {
-    showElement(nextButton);
-    hideElement(quizPage);
-    showElement(welcomePage);
-
-    quizSelectionForm.reset();
     quizData = {};
-    document.getElementById("quiz").remove();
+    quizPage.childNodes.forEach((element) => element.remove());
+    resultPanel.childNodes.forEach((element) => element.remove());
+
+    hideElement(submitButton);
+    hideElement(quizPage);
+    hideElement(resultPanel);
+    showElement(nextButton);
+    showElement(welcomePage);
 });
 
 nextButton.addEventListener("click", (event) => {
+    quizData = {};
     for (let i = 0; i < 6; i++) {
         if (moduleSelectBox[i].checked) {
             quizData = { ...quizData, ...modules[i] }
@@ -64,12 +72,56 @@ nextButton.addEventListener("click", (event) => {
 
     if (quizData.length == 0) return;
 
+    shuffle(quizData)
+    quizPage.childNodes.forEach((element) => element.remove());
+    quizPage.appendChild(generateQuiz());
+
+    resultPanel.childNodes.forEach((element) => element.remove());
     hideElement(nextButton);
     hideElement(welcomePage);
+    hideElement(resultPanel);
+    showElement(submitButton);
     showElement(quizPage);
+});
 
-    shuffle(quizData)
-    quizPage.appendChild(generateQuiz());
+
+
+submitButton.addEventListener("click", (event) => {
+    ++attemptNum;
+
+    const quiz = document.getElementById("quiz");
+    quiz.setAttribute("class", "submitted");
+
+    const questions = quiz.getElementsByClassName("question");
+    let correctAnswers = 0;
+    for (let question of questions) {
+        let isCorrect = true;
+        for (let choice of question.getElementsByTagName("input")) {
+            choice.disabled = true;
+            if (!((choice.className == "correct") == choice.checked)) {
+                isCorrect = false;
+            }
+        }
+        if (isCorrect) {
+            ++correctAnswers;
+        }
+    }
+
+    p = document.createElement("p");
+    p.id = "result";
+    const accuracy = 100 * correctAnswers / questions.length;
+    result = `${correctAnswers}/${questions.length} (${accuracy.toFixed(2)}%)`;
+    p.appendChild(document.createTextNode(result));
+    resultPanel.appendChild(p);
+
+    const H = 2 * Math.max(accuracy - 50, 0);
+    const S = 80;
+    const L = 60 + 1 / 5 * Math.max(accuracy - 50, 0);
+    resultPanel.style.backgroundColor = `hsl(${H}, ${S}%, ${L}%)`;
+
+    hideElement(submitButton);
+    showElement(nextButton);
+    showElement(resultPanel);
 });
 
 function shuffle(array) {
@@ -86,15 +138,18 @@ function shuffle(array) {
 
 function generateQuiz() {
     const number = questionNum.value
-    if (number == "ALL") {
-        sample = quizData
-    } else {
-        sample = quizData.slice(0, number);
+    if (number != "ALL") {
+        quizData = quizData.slice(0, number);
     }
 
     div = document.createElement("div")
     div.id = "quiz"
-    sample.forEach((question, questionIndex) => {
+
+    heading = document.createElement("h1")
+    heading.appendChild(document.createTextNode(`Attempt ${attemptNum}`));
+    div.appendChild(heading)
+
+    quizData.forEach((question, questionIndex) => {
         div.appendChild(generateQuestion(question, questionIndex))
     });
     return div;
@@ -106,7 +161,8 @@ function generateQuestion(question, questionIndex) {
     const isMultiSelect = question[1].multi_select;
 
     const div = document.createElement("div");
-    div.id = "question";
+    div.id = "Q" + questionIndex;
+    div.setAttribute("class", "question");
 
     const p = document.createElement("p")
 
@@ -123,7 +179,7 @@ function generateQuestion(question, questionIndex) {
         const input = document.createElement("input")
         input.type = isMultiSelect ? "checkbox" : "radio";
         input.name = "Q" + questionIndex;
-        input.id = input.name + "/" + choiceIndex;
+        input.id = "Q" + questionIndex + "/" + choiceIndex;
         input.setAttribute("class", isCorrect ? "correct" : "incorrect");
         div.appendChild(input);
 
@@ -139,3 +195,4 @@ function generateQuestion(question, questionIndex) {
 
     return div;
 }
+
