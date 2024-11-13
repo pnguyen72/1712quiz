8063eadd097d6be2a1b40cf78b44cb1645a12c33
Felix Nguyen <pnguyen72@my.bcit.ca>
2024-11-13 00:31:14 -0800

    Shorten the explanation placeholder on smaller screen

--- scripts/globalVar.js
+++ scripts/globalVar.js
@@ -5,4 +5,4 @@ let formChanged = false;
 let newQuizNeeded = true;
 let disagreeNum = 0;
 let moduleSelectBoxes = [];
-const placeholderExplanation = "No explanation available. Why don't you add one?"
+const placeholderExplanation = `No explanation available. <span id="placeholder-expansion">Why don't you add one?</span>`;
