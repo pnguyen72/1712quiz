74e681f56338ab6b549656cf59df51073e8cd01c
Felix Nguyen <pnguyen72@my.bcit.ca>
2025-01-01 18:21:04 -0800

    Update attempt table styles

--- scripts/generators.js
+++ scripts/generators.js
@@ -256,6 +256,8 @@ function updateAttemptsTable() {
       const modules = document.createElement("td");
       const duration = document.createElement("td");
       const result = document.createElement("td");
+      const resultScore = document.createElement("span");
+      const resultPercentage = document.createElement("span");
 
       timestamp.className = "timestamp";
       timestamp.setAttribute("value", attempt.timestamp);
@@ -274,12 +276,19 @@ function updateAttemptsTable() {
       duration.className = "duration";
       duration.innerText = attempt.duration;
 
+      resultScore.className = "score";
+      resultScore.innerText = `${score}/${outOf}`;
+
+      resultPercentage.className = "percentage";
+      resultPercentage.innerText = ` (${roundedAccuracy}%)`;
+
       result.className = "result";
-      result.innerText = `${score}/${outOf} (${roundedAccuracy}%)`;
       result.style.backgroundColor = `hsla(${H}, ${S}%, ${L}%, ${0.75})`;
       if (darkModeToggle.checked) {
         result.style.color = L < 61 ? "#eee" : "#000";
       }
+      result.appendChild(resultScore);
+      result.appendChild(resultPercentage);
 
       row.className = "row";
       row.setAttribute("exam", attempt.exam);
