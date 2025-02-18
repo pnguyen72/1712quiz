9064c1582821016f07e046690c4c3e7164c5f1a4
Felix Nguyen <pnguyen72@my.bcit.ca>
2025-02-18 02:36:25 -0800

    Refactor loading explanation resources

--- scripts/data.js
+++ scripts/data.js
@@ -7,21 +7,6 @@ const knowledge = localStorage.getItem("knowledge")
   ? JSON.parse(localStorage.getItem("knowledge"))
   : {};
 
-var converter = null;
-
-function loadExplanationResources() {
-  if (typeof loadFirebase != "undefined") {
-    loadFirebase();
-  }
-
-  if (converter) return;
-  converter = true;
-  fetch("https://unpkg.com/showdown@2.1.0/dist/showdown.min.js")
-    .then((res) => res.text())
-    .then((src) => eval(src))
-    .then(() => (converter = new showdown.Converter()));
-}
-
 function loadQuestions() {
   unfinishedAttempts.load();
   const promises = [];
